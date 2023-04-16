f65324c69b77b9061c95671a58b6488f26701405
utkarsh8115 <utkarsh>
2023-04-16 13:28:29 +0530

    No change required

---
+++ main.py
@@ -0,0 +1,26 @@
+print ("hello world")
+print (97)
+print ("how are you")
+print (89*98)
+print (878/8)
+print (98+88)
+print ("hello my name is Son \nmy father's name is \"Father\"", end="and then")  
+print (" hello", 8,5, sep="~")
+a= 987
+b= 878
+c= 30
+print(a*b*c)
+print (type (a))
+list1= [8,2, [-4, 5], ["apple", "banana"]]
+print (list1)
+tuple1= [8,2, [-4, 5], ["apple", "banana"]]
+print (tuple1)
+dict1 = {"name":"Sakshi", "age":30, "canVote":True}
+print (dict1)
+print ("the value of a+b is =", a+b )
+print ("the value of a-b is =", a-b )
+print ("the value of a*b is =", a*b )
+print ("the value of a/b is =", a/b )
+d= "9"
+e= "60"
+print (int(d)+int(e))
